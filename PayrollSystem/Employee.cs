4a41dc1e788201122575f5387e3224a94c2ebb60
alexesca <alexander.escamilla.moreno@gmail.com>
2016-11-02 10:06:30 -0600

    Adding the classes to the Project

---
+++ PayrollSystem/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace PayrollSystem
+{
+	public class Employee
+	{
+
+		public String EmployeeID
+		{
+			get
+			{
+				return employee_ID;
+			}
+			set
+			{
+				employee_ID = value;
+			}
+		}
+
+		public String EmployeeName
+		{
+			get
+			{
+				return employee_Name;
+			}
+			set
+			{
+				employee_Name = value;
+			}
+
+		}
+
+		public String EmployeeAddress
+		{
+			get
+			{
+				return employee_Address;
+			}
+
+			set
+			{
+				employee_Address = value;
+			}
+		}
+		public String EmployeePaymethod
+		{
+			get
+			{
+				return employee_Paymethod;
+			}
+			set
+			{
+				employee_Paymethod = value;
+			}
+		}
+
+		public String EmployeeType
+		{
+			get
+			{
+				return employee_Type;
+			}
+			set
+			{
+				employee_Type = value;
+			}
+		}
+
+		protected String employee_ID;
+		protected String employee_Name;
+		protected String employee_Address;
+		protected String employee_Paymethod;
+		protected String employee_Type;
+
+
+	}
+}
