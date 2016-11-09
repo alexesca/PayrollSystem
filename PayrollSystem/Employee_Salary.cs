f81c8152d5bd9605d9065de88eaae68c2acb4e22
Bethaly <Bethaly@LAPTOP-5I54J8PB>
2016-11-09 00:17:01 -0700

    It is not finish, I was testing

--- PayrollSystem/Employee_Salary.cs
+++ PayrollSystem/Employee_Salary.cs
@@ -3,32 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/* Payroll System - Commission Employee
+ * Miranda Motter
+ * 8 Nov 2016
+ * Creates a subclass of Employee for Salary
+ * Professor Masterson, CS 240-1
+ */
 
 namespace PayrollSystem
 {
     class Employee_Salary : Employee
     {
-        private string payMethod { get; set; }
+        private string paymethod { get; set; }
 
-        Employee_Salary()
+        public Employee_Salary()
         {
 
         }
-
-        public string PaymentMethod (String payMethod)
+        
+        private string PayMethod (string paymethod)
         {
-            if (employee_Paymethod == "Email")
-            {
-                return payMethod= "It will sent to this addres";
-            }
-            if (employee_Paymethod == "Direct Deposit")
-            {
-                return payMethod = "Sent to Back";
-            }
-            return payMethod;
+            if (paymethod =="check")
+            return 
         }
-
-      
     }
 }
 
